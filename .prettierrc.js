51f519994a5e1cc8947a5bbc25ebfff0672bcc64
Grant Gryczan <grantgryczan@gmail.com>
2023-11-04 15:50:10 -0400

    Add Prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+// @ts-check
+
+/** @type {import("prettier").Config} */
+export default {}; // NO CHANGES ALLOWED! See below comment.
+
+// To understand the benefit of sticking with Prettier's defaults (even if
+// they're suboptimal), see https://prettier.io/docs/en/why-prettier.
