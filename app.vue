b394cb36d8a86872acec0505ed760bfc4b832540
Grant Gryczan <grantgryczan@gmail.com>
2023-11-03 20:47:36 -0400

    Extract font code to component

--- app.vue
+++ app.vue
@@ -22,22 +22,8 @@ useSeoMeta({
 
 <template>
   <div id="app">
-    <Head>
-      <!-- Copied from https://fonts.google.com/specimen/Roboto. -->
-      <Link rel="preconnect" href="https://fonts.googleapis.com" />
-      <Link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="" />
-      <Link
-        href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
-        rel="stylesheet"
-      />
-    </Head>
+    <TheFont />
 
     <NuxtPage />
   </div>
 </template>
-
-<style lang="scss">
-:root {
-  font-family: "Roboto", sans-serif;
-}
-</style>
---
+++ components/TheFont.vue
@@ -0,0 +1,17 @@
+<template>
+  <Head>
+    <!-- Copied from https://fonts.google.com/specimen/Roboto. -->
+    <Link rel="preconnect" href="https://fonts.googleapis.com" />
+    <Link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="" />
+    <Link
+      href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
+      rel="stylesheet"
+    />
+  </Head>
+</template>
+
+<style lang="scss">
+:root {
+  font-family: "Roboto", sans-serif;
+}
+</style>
