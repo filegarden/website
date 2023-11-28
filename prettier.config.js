5558efc2957a0e41210aaca36f08514d98c5ec28
Grant Gryczan <grantgryczan@gmail.com>
2023-11-27 22:37:15 -0500

    Add emoji to draw attention to comment

--- prettier.config.js
+++ prettier.config.js
@@ -1,7 +1,7 @@
 // @ts-check
 
 /** @type {import("prettier").Config} */
-export default {}; // NO CHANGES ALLOWED! See below comment.
+export default {}; // ⚠️ NO CHANGES ALLOWED! See below comment.
 
 // To understand the benefit of sticking with Prettier's defaults (even if
 // they're suboptimal), see https://prettier.io/docs/en/why-prettier.
