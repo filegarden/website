35dfe25c4c967bd2b2db5d3bfcafc66380ea754d
Grant Gryczan <grantgryczan@gmail.com>
2024-08-10 03:38:28 -0400

    Add button component

---
+++ src/components/Button.vue
@@ -0,0 +1,39 @@
+<template>
+  <button type="button">
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped lang="scss">
+button {
+  line-height: 1.2;
+  font-size: 1rem;
+  font-family: var(--font-family);
+  color: var(--text-color);
+
+  padding: 0.5rem 1.5rem;
+  background-color: var(--surface-color);
+  border: 1px solid var(--border-color);
+  border-radius: 0.75rem;
+
+  cursor: pointer;
+  white-space: nowrap;
+  overflow: hidden;
+  user-select: none;
+
+  transition:
+    0.05s ease-out border-color,
+    0.05s ease-out box-shadow;
+
+  &:hover {
+    border-color: var(--hover-border-color);
+    box-shadow: inset 0 0 0.25rem -1px var(--border-color);
+  }
+
+  &:active,
+  &:focus-visible {
+    border-color: var(--active-border-color);
+    box-shadow: inset 0 0 0.5rem -1px var(--border-color);
+  }
+}
+</style>
