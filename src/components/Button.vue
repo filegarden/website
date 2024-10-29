0749e74e78597aa42fe1b8456be7f4d26dce91f7
Grant Gryczan <grantgryczan@gmail.com>
2024-10-29 03:56:39 -0400

    Improve button feel

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -14,8 +14,7 @@ button {
   padding: 0.5rem 1.5rem;
   background-color: var(--surface-color);
   border: 1px solid var(--border-color);
-  // This size is about a fourth of a typical button's height.
-  border-radius: 0.581rem;
+  border-radius: 0.667rem;
 
   cursor: pointer;
   white-space: nowrap;
@@ -23,18 +22,22 @@ button {
   user-select: none;
 
   transition:
-    0.05s ease-out border-color,
-    0.05s ease-out box-shadow;
+    0.1s ease-out border-color,
+    0.1s ease-out box-shadow;
 
   &:enabled:hover {
     border-color: var(--hover-border-color);
-    box-shadow: inset 0 0 0.25rem -1px var(--border-color);
+    box-shadow:
+      inset 0 0 0.25rem -1px var(--border-color),
+      2px 1px 6px var(--shadow-color);
   }
 
   &:enabled:active,
   &:enabled:focus-visible {
     border-color: var(--active-border-color);
-    box-shadow: inset 0 0 0.5rem -1px var(--border-color);
+    box-shadow:
+      inset 0 0 0.5rem -1px var(--border-color),
+      2px 1px 12px var(--shadow-color);
   }
 
   &:disabled {
