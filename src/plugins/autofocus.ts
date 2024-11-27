6e29cf106b52c4d08bef8dbcfb549526f5960162
Grant Gryczan <grantgryczan@gmail.com>
2024-11-27 11:09:35 -0500

    Accept value in `v-autofocus` directive

--- src/plugins/autofocus.ts
+++ src/plugins/autofocus.ts
@@ -1,12 +1,16 @@
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.directive("autofocus", {
-    getSSRProps() {
-      return { autofocus: true };
+  nuxtApp.vueApp.directive<HTMLElement, boolean | undefined>("autofocus", {
+    getSSRProps({ value }) {
+      return { autofocus: value !== false };
     },
 
-    mounted(el: HTMLElement) {
-      el.autofocus = true;
-      el.focus();
+    mounted(el, { value }) {
+      const autofocus = value !== false;
+
+      el.autofocus = autofocus;
+      if (autofocus) {
+        el.focus();
+      }
     },
   });
 });
