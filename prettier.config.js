9066bb5b6b39f7519fe1e387d5d7cf83c2a667c4
Grant Gryczan <grantgryczan@gmail.com>
2025-02-25 17:44:55 -0500

    Clean up project root by removing unneeded files

--- prettier.config.js
+++
@@ -1,8 +0,0 @@
-// @ts-check
-
-/** @type {import("prettier").Config} */
-export default {}; // ⚠️ NO CHANGES ALLOWED! See below comment.
-
-// To understand the benefit of sticking with Prettier's defaults (even if
-// they're suboptimal), see https://prettier.io/docs/en/why-prettier and
-// https://prettier.io/docs/en/option-philosophy.
