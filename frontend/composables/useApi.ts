5065dc38cef6ca2b212bebc88935b22a4f4550ef
Grant Gryczan <grantgryczan@gmail.com>
2025-03-01 14:30:33 -0500

    Show error boxes for errors during SSR There isn't a great way to catch these errors and prevent an error box yet though.

--- frontend/composables/useApi.ts
+++ frontend/composables/useApi.ts
@@ -1,4 +1,5 @@
 import type { UseFetchOptions } from "nuxt/app";
+import { createFetchError } from "ofetch";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any -- We don't have an accurate type for this, and using `unknown` would require many pointless type assertions or runtime checks.
 type ResT = any;
@@ -11,7 +12,15 @@ export default function useApi(
   url: MaybeRefOrGetter<string>,
   options?: UseFetchOptions<ResT>,
 ) {
+  const errorBoxes = useErrorBoxes();
+
   return useFetch<ResT>(url, {
+    onResponseError(ctx) {
+      const error = createFetchError(ctx);
+
+      errorBoxes.handleError(error);
+    },
+
     ...options,
     $fetch: api,
   });
