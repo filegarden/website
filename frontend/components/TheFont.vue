7f56c60bedd85eaaa28382efde51cea754297473
Grant Gryczan <grantgryczan@gmail.com>
2025-03-04 15:59:49 -0500

    Fix inconsistent font weight/style rendering

--- frontend/components/TheFont.vue
+++ frontend/components/TheFont.vue
@@ -4,7 +4,7 @@
     <Link rel="preconnect" href="https://fonts.googleapis.com" />
     <Link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="" />
     <Link
-      href="https://fonts.googleapis.com/css2?family=Roboto&family=Roboto+Mono&display=swap"
+      href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
       rel="stylesheet"
     />
   </Head>
