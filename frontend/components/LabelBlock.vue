72f62da99475764e5e7fc54a8d23122f7aafe0d6
Grant Gryczan <grantgryczan@gmail.com>
2025-04-05 01:03:07 -0400

    Update `LabelBlock` class name

--- frontend/components/LabelBlock.vue
+++ frontend/components/LabelBlock.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="label-wrapper">
+  <div class="label-block">
     <slot></slot>
   </div>
 </template>
 
 <style scoped lang="scss">
-.label-wrapper {
+.label-block {
   margin: 0.25em 1px;
   opacity: 0.875;
 }
