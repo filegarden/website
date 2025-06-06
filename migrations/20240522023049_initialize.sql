f416cbfc2aeacd80c0403e1f67ee122665a65889
Grant Gryczan <grantgryczan@gmail.com>
2025-06-05 23:14:09 -0400

    Fix excessive timestamp resolution

--- migrations/20240522023049_initialize.sql
+++ migrations/20240522023049_initialize.sql
@@ -1,20 +1,28 @@
 CREATE EXTENSION citext;
 
+-- `timestamptz` has microsecond resolution by default, which needlessly
+-- increases the attack surface for timing attacks. `timestamptz(3)` only has
+-- millisecond resolution and should always be used instead.
+--
+-- TODO: Something in CI should guarantee this, and that should have a code
+-- comment instead of this database migration comment, because migrations can't
+-- be edited once deployed.
+
 CREATE TABLE terms_version (
     constrain_table_to_one_row boolean NOT NULL UNIQUE DEFAULT TRUE
         CHECK (constrain_table_to_one_row),
-    updated_at timestamptz PRIMARY KEY
+    updated_at timestamptz(3) PRIMARY KEY
         GENERATED ALWAYS AS (GREATEST(terms_updated_at, privacy_updated_at))
         STORED,
-    terms_updated_at timestamptz NOT NULL DEFAULT now(),
-    privacy_updated_at timestamptz NOT NULL DEFAULT now(),
+    terms_updated_at timestamptz(3) NOT NULL DEFAULT now(),
+    privacy_updated_at timestamptz(3) NOT NULL DEFAULT now(),
     terms_hash bytea NOT NULL,
     privacy_hash bytea NOT NULL
 );
 
 CREATE TABLE users (
-    created_at timestamptz NOT NULL DEFAULT now(),
-    accepted_terms_at timestamptz NOT NULL,
+    created_at timestamptz(3) NOT NULL DEFAULT now(),
+    accepted_terms_at timestamptz(3) NOT NULL,
     id bytea PRIMARY KEY,
     email citext NOT NULL UNIQUE,
     name text NOT NULL,
@@ -23,9 +31,9 @@ CREATE TABLE users (
 );
 
 CREATE TABLE unverified_emails (
-    created_at timestamptz NOT NULL DEFAULT now(),
+    created_at timestamptz(3) NOT NULL DEFAULT now(),
     token_hash bytea PRIMARY KEY,
-    user_accepted_terms_at timestamptz NOT NULL DEFAULT now(),
+    user_accepted_terms_at timestamptz(3) NOT NULL DEFAULT now(),
     user_id bytea UNIQUE REFERENCES users (id) ON DELETE CASCADE,
     email citext NOT NULL,
     code_hash text
@@ -35,14 +43,14 @@ CREATE UNIQUE INDEX unverified_user_emails ON unverified_emails (email)
     WHERE user_id IS NULL;
 
 CREATE TABLE password_resets (
-    created_at timestamptz NOT NULL DEFAULT now(),
+    created_at timestamptz(3) NOT NULL DEFAULT now(),
     token_hash bytea PRIMARY KEY,
     user_id bytea NOT NULL UNIQUE REFERENCES users (id) ON DELETE CASCADE
 );
 
 CREATE TABLE sessions (
-    created_at timestamptz NOT NULL DEFAULT now(),
-    accessed_at timestamptz NOT NULL DEFAULT now(),
+    created_at timestamptz(3) NOT NULL DEFAULT now(),
+    accessed_at timestamptz(3) NOT NULL DEFAULT now(),
     token_hash bytea PRIMARY KEY,
     user_id bytea NOT NULL REFERENCES users (id) ON DELETE CASCADE
 );
@@ -53,8 +61,8 @@ CREATE INDEX sessions_by_user_id ON sessions (user_id);
 CREATE TYPE encoding AS ENUM ('br');
 
 CREATE TABLE files (
-    created_at timestamptz NOT NULL DEFAULT now(),
-    modified_at timestamptz NOT NULL DEFAULT now(),
+    created_at timestamptz(3) NOT NULL DEFAULT now(),
+    modified_at timestamptz(3) NOT NULL DEFAULT now(),
     id bytea PRIMARY KEY,
     name text NOT NULL,
     owner_id bytea NOT NULL REFERENCES users (id) ON DELETE CASCADE,
@@ -73,7 +81,7 @@ CREATE TABLE files (
 CREATE INDEX files_by_id_path ON files (owner_id, parent_id_path, id);
 
 CREATE TABLE folders (
-    created_at timestamptz NOT NULL DEFAULT now(),
+    created_at timestamptz(3) NOT NULL DEFAULT now(),
     id bytea PRIMARY KEY,
     name text NOT NULL,
     owner_id bytea NOT NULL REFERENCES users (id) ON DELETE CASCADE,
