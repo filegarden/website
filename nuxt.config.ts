4bc63285898d914dca18259bd879f51bdcf0598b
Grant Gryczan <grantgryczan@gmail.com>
2025-06-07 07:59:01 -0400

    Reorganize Nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-06-06",
   devtools: { enabled: true },
-  // IDE debuggers need source maps to work.
-  sourcemap: true,
   srcDir: "frontend",
   modules: ["@nuxt/eslint", "nuxt-compile-markdown"],
+
+  // IDE debuggers need source maps to work.
+  sourcemap: true,
 
   routeRules: {
     "/discord": { redirect: "https://discord.gg/fWexzeh" },
