538160591cb1f7a73d3365d89b0fe27e661eda80
Grant Gryczan <grantgryczan@gmail.com>
2025-06-18 19:48:48 -0400

    Add error page

---
+++ frontend/error.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import type { NuxtError } from "#app";
+
+defineProps<{
+  /** The error passed into the page by Nuxt. */
+  error: NuxtError;
+}>();
+</script>
+
+<template>
+  <SmallPanelLayout>
+    <h1>Error {{ error.statusCode }}</h1>
+
+    <p>{{ error.message }}</p>
+
+    <template #bottom-text>
+      <p>
+        <A href="/">Go Home</A>
+      </p>
+    </template>
+  </SmallPanelLayout>
+</template>
+
+<style scoped lang="scss">
+:deep(main) {
+  text-align: center;
+}
+</style>
