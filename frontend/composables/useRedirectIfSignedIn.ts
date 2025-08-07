16007462fc098fc8d768ae6bd34c289e025f550f
Grant Gryczan <grantgryczan@gmail.com>
2025-08-07 00:28:05 -0400

    Don't use `never` because it breaks `finally` Throwing a `silence`d error works much better.

--- frontend/composables/useRedirectIfSignedIn.ts
+++ frontend/composables/useRedirectIfSignedIn.ts
@@ -2,11 +2,11 @@
  * If the user is signed in, redirects to the relative URL in the route's `to`
  * query, or to the user's files if there is no valid `to` query.
  *
- * If a redirect should occur, returns `true` server-side, or never returns
+ * If a redirect should occur, returns `true` server-side, or throws silently
  * client-side since the caller won't need to continue after redirecting to
- * another page (see below note). If no redirect should occur, returns `false`.
+ * another page. If no redirect should occur, returns `false`.
  *
- * Note that the redirect is client-side-only so the user's sign-in status isn't
+ * Note that the redirect is client-side so the user's sign-in status isn't
  * leaked to other websites, since that would allow them to fingerprint our
  * users more precisely. See https://robinlinus.github.io/socialmedia-leak/.
  */
@@ -36,5 +36,5 @@ export default async function useRedirectIfSignedIn(): Promise<boolean> {
   await preventLeaveConfirmations(() =>
     navigateTo(toRelativeUrl, { replace: true }),
   );
-  return never();
+  throw silence(new Error("Redirected by `useRedirectIfSignedIn`"));
 }
--- frontend/utils/never.ts
+++
@@ -1,6 +0,0 @@
-/** Returns a promise that never settles. */
-export default function never(): Promise<never> {
-  return new Promise(() => {
-    // Simply don't resolve the promise.
-  });
-}
