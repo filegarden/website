ba6ea6372d8d4bb0e59ba3d465ccdfce9af3cd28
Grant Gryczan <grantgryczan@gmail.com>
2025-08-07 00:12:51 -0400

    Make `never` doc comment more concise

--- frontend/utils/never.ts
+++ frontend/utils/never.ts
@@ -1,4 +1,4 @@
-/** Returns a promise that's never resolved or rejected. */
+/** Returns a promise that never settles. */
 export default function never(): Promise<never> {
   return new Promise(() => {
     // Simply don't resolve the promise.
