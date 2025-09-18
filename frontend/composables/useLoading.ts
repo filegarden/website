1e2bd428aca88069400cd3a32f4cb488ed4d8287
Grant Gryczan <grantgryczan@gmail.com>
2025-09-18 05:58:04 -0400

    Fix infinite effect loop from starting loading This happened because `++` both tracks and triggers, which causes an infinite update loop when used in `watchEffect`.

--- frontend/composables/useLoading.ts
+++ frontend/composables/useLoading.ts
@@ -10,18 +10,39 @@ export default function useLoading(
 }
 
 export class LoadingState {
+  /** How many things are currently loading. */
+  protected count = 0;
+  protected readonly track: () => void;
+  protected readonly trigger: () => void;
+
   protected constructor(
     protected overrideLoading: MaybeRefOrGetter<boolean> = false,
   ) {
     markRaw(this);
-  }
 
-  /** How many things are currently loading. */
-  protected count = ref(0);
+    let track: () => void = undefined as never;
+    let trigger: () => void = undefined as never;
+
+    // See `start` method for why a `customRef` is needed.
+    customRef((trackArg, triggerArg) => {
+      track = trackArg;
+      trigger = triggerArg;
+
+      function forbidUse() {
+        throw new Error("`LoadingState`'s ref shouldn't be used directly");
+      }
+
+      return { get: forbidUse, set: forbidUse };
+    });
+
+    this.track = track;
+    this.trigger = trigger;
+  }
 
   /** Whether the loading state is active. */
   get value(): boolean {
-    return this.count.value !== 0 || toValue(this.overrideLoading);
+    this.track();
+    return this.count !== 0 || toValue(this.overrideLoading);
   }
 
   /**
@@ -29,16 +50,24 @@ export class LoadingState {
    * of the loading state.
    */
   start() {
+    // `++` on a normal ref's value would both track and trigger, which would
+    // cause an infinite update loop in `watchEffect` for example. Abstractly,
+    // it doesn't make sense for starting/stopping loading to count as reading
+    // the loading state, so this triggers without tracking using a custom ref.
+    this.count++;
+    this.trigger();
+
     let stopped = false;
-    this.count.value++;
 
     const stop = () => {
       if (stopped) {
         throw new Error("Loading can't stop because it was already stopped");
       }
 
-      this.count.value--;
       stopped = true;
+
+      this.count--;
+      this.trigger();
     };
 
     return stop;
@@ -49,12 +78,12 @@ export class LoadingState {
    * the callback completes.
    */
   async during(callback: () => Promise<void>): Promise<void> {
-    this.count.value++;
+    const stop = this.start();
 
     try {
       await callback();
     } finally {
-      this.count.value--;
+      stop();
     }
   }
 }
