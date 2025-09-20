24b9cfd9142e86f877a2ade3655d1d59d3927aa4
Grant Gryczan <grantgryczan@gmail.com>
2025-09-19 20:23:17 -0400

    Add UI for configuring TOTP

---
+++ frontend/utils/printNode.ts
@@ -0,0 +1,24 @@
+export default async function printHtml(node: Node) {
+  const iframe = document.createElement("iframe");
+  iframe.hidden = true;
+  iframe.srcdoc =
+    '<!DOCTYPE html><html><head><meta charset="utf8"></head><body></body></html>';
+
+  document.body.append(iframe);
+
+  await new Promise((resolve) => {
+    iframe.addEventListener("load", resolve);
+  });
+
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion -- The window is non-nullable once loaded.
+  const contentWindow = iframe.contentWindow!;
+  contentWindow.document.body.append(node);
+
+  contentWindow.print();
+
+  await new Promise((resolve) => {
+    contentWindow.addEventListener("afterprint", resolve);
+  });
+
+  iframe.remove();
+}
