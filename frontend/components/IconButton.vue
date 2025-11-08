41d808db3ed8fcfe1667d166bf9b7c8158e9a4ee
Grant Gryczan <grantgryczan@gmail.com>
2025-11-08 18:41:39 -0500

    Fix icon button dependence on stylesheet order

--- frontend/components/IconButton.vue
+++ frontend/components/IconButton.vue
@@ -17,67 +17,69 @@ const { label } = defineProps<{
 </template>
 
 <style lang="scss" scoped>
-.icon-button {
-  vertical-align: middle;
-  font-size: 1em;
-  font-family: var(--font-family);
-  color: var(--color-text);
+@layer base {
+  .icon-button {
+    vertical-align: middle;
+    font-size: 1em;
+    font-family: var(--font-family);
+    color: var(--color-text);
 
-  position: relative;
-  width: 2em;
-  height: 2em;
-  padding: 0;
+    position: relative;
+    width: 2em;
+    height: 2em;
+    padding: 0;
 
-  background: none;
-  border: none;
-  border-radius: 0;
+    background: none;
+    border: none;
+    border-radius: 0;
 
-  cursor: pointer;
+    cursor: pointer;
 
-  opacity: 0.875;
-  transition: 0.1s ease-out opacity;
+    opacity: 0.875;
+    transition: 0.1s ease-out opacity;
 
-  &:hover:not(:disabled),
-  &:active:not(:disabled),
-  &:focus-visible:not(:disabled) {
-    opacity: 1;
-  }
-}
-
-.icon {
-  transition: 0.1s ease-out filter;
-
-  .icon-button:hover &,
-  .icon-button:focus-visible &,
-  .icon-button:active & {
-    filter: brightness(1.125);
-  }
-}
-
-.icon-glow {
-  position: absolute;
-  width: 100%;
-  pointer-events: none;
-
-  opacity: 0;
-  transition:
-    0.1s ease-out opacity,
-    0.1s ease-out filter;
-
-  .icon-button:hover & {
-    opacity: 0.25;
-    filter: blur(0.25em);
+    &:hover:not(:disabled),
+    &:active:not(:disabled),
+    &:focus-visible:not(:disabled) {
+      opacity: 1;
+    }
   }
 
-  .icon-button:focus-visible &,
-  .icon-button:active & {
-    opacity: 0.4375;
-    filter: blur(0.2813em);
-    transition-duration: 0.067s, 0.067s;
-  }
-}
+  .icon {
+    transition: 0.1s ease-out filter;
 
-.icon-button :deep(svg) {
-  vertical-align: bottom;
+    .icon-button:hover &,
+    .icon-button:focus-visible &,
+    .icon-button:active & {
+      filter: brightness(1.125);
+    }
+  }
+
+  .icon-glow {
+    position: absolute;
+    width: 100%;
+    pointer-events: none;
+
+    opacity: 0;
+    transition:
+      0.1s ease-out opacity,
+      0.1s ease-out filter;
+
+    .icon-button:hover & {
+      opacity: 0.25;
+      filter: blur(0.25em);
+    }
+
+    .icon-button:focus-visible &,
+    .icon-button:active & {
+      opacity: 0.4375;
+      filter: blur(0.2813em);
+      transition-duration: 0.067s, 0.067s;
+    }
+  }
+
+  .icon-button :deep(svg) {
+    vertical-align: bottom;
+  }
 }
 </style>
