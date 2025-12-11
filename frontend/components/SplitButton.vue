61042628fd9f195402bc3d0830024c4692b5eab4
Grant Gryczan <grantgryczan@gmail.com>
2025-12-11 17:30:12 -0500

    Fix split button style duplication

--- frontend/components/SplitButton.vue
+++ frontend/components/SplitButton.vue
@@ -11,11 +11,12 @@ const { popoverButtonAriaLabel, accented } = defineProps<{
 <template>
   <fieldset class="split-button" :class="{ accented }">
     <slot name="button"></slot>
-    <Button class="popover-button" :aria-label="popoverButtonAriaLabel">
-      <IconChevronDown />
-    </Button>
 
-    <div class="popover-button-background"></div>
+    <div class="popover-button-container">
+      <Button class="popover-button" :aria-label="popoverButtonAriaLabel">
+        <IconChevronDown />
+      </Button>
+    </div>
   </fieldset>
 </template>
 
@@ -28,7 +29,7 @@ $popover-button-width: calc(2 * $popover-button-padding-x + 1em);
 .split-button {
   position: relative;
   display: inline-flex;
-  gap: $gap;
+  padding-right: calc($popover-button-width + $gap);
 
   // Prevent outside elements from covering any `z-index: -1` descendants.
   isolation: isolate;
@@ -51,42 +52,23 @@ $popover-button-width: calc(2 * $popover-button-padding-x + 1em);
   background-size: calc(100% + $popover-button-width) 100%;
 }
 
+.popover-button-container {
+  container: split-button / size;
+  position: absolute;
+  inset: 0;
+  pointer-events: none;
+
+  text-align: right;
+}
+
 .popover-button {
+  pointer-events: auto;
   padding: 0 $popover-button-padding-x;
 
   border-top-left-radius: $inner-border-radius;
   border-bottom-left-radius: $inner-border-radius;
 
-  background: none;
-}
-
-.popover-button-background {
-  container: split-button / size;
-  position: absolute;
-  inset: 0;
-  z-index: -1;
-
-  &::after {
-    content: "";
-    position: absolute;
-    right: 0;
-    width: $popover-button-width;
-    height: 100%;
-
-    // TODO: Don't hard-code this being copied from button styles.
-    border-radius: 0.6em;
-    border-top-left-radius: $inner-border-radius;
-    border-bottom-left-radius: $inner-border-radius;
-
-    background-color: var(--color-foreground);
-    // TODO: Don't hard-code this being copied from button styles.
-    background-image: linear-gradient(
-      135deg,
-      var(--color-foreground-light),
-      var(--color-foreground)
-    );
-    background-size: 100cqw 100%;
-    background-position: right;
-  }
+  background-size: 100cqw 100%;
+  background-position: right;
 }
 </style>
