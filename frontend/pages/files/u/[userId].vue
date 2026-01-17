ce1fc58bc8d06ff85650353ced91eec871c5aed1
Grant Gryczan <grantgryczan@gmail.com>
2026-01-17 02:47:47 -0500

    Create incomplete files page

---
+++ frontend/pages/files/u/[userId].vue
@@ -0,0 +1,109 @@
+<script setup lang="ts">
+useTitle("Files");
+</script>
+
+<template>
+  <div class="page">
+    <header class="header">
+      <div class="header-sidebar">
+        <NavLogo class="logo" />
+      </div>
+
+      <div class="account-button-wrapper">
+        <AccountMenu teleport-menu-to="#account-menu-wrapper" />
+      </div>
+    </header>
+
+    <div id="account-menu-wrapper"></div>
+
+    <div class="below-header">
+      <nav class="sidebar">
+        <SplitButton
+          class="upload-button"
+          menu-label="More Upload Options"
+          accented
+        >
+          <template #button>
+            <Button>Upload</Button>
+          </template>
+
+          <MenuButton v-autofocus role="menuitem" tabindex="-1">
+            <IconDocument />
+            File upload
+          </MenuButton>
+
+          <MenuButton role="menuitem" tabindex="-1">
+            <IconFolder />
+            Folder upload
+          </MenuButton>
+
+          <hr />
+
+          <MenuButton role="menuitem" tabindex="-1">
+            <IconFolderPlus />
+            New folder
+          </MenuButton>
+        </SplitButton>
+      </nav>
+
+      <div class="file-pane"></div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+$sidebar-width: 16rem;
+
+.page {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+
+.header {
+  width: 100%;
+  height: 4rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.header-sidebar {
+  width: $sidebar-width;
+}
+
+.logo {
+  font-size: 1.75rem;
+  margin-left: 1.125rem;
+}
+
+.account-button-wrapper {
+  margin: 0 0.75rem;
+}
+
+#account-menu-wrapper {
+  position: relative;
+  margin-right: 0.5rem;
+}
+
+.below-header {
+  flex-grow: 1;
+  display: flex;
+}
+
+.sidebar {
+  width: $sidebar-width;
+  padding: 1rem;
+}
+
+.upload-button {
+  width: 100%;
+  font-size: 1.25rem;
+}
+
+.file-pane {
+  flex-grow: 1;
+  position: relative;
+}
+</style>
