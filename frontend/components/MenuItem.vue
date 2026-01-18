fc320080ef93ed10f1c22b8b084fe5066a253ea5
Grant Gryczan <grantgryczan@gmail.com>
2026-01-17 22:27:46 -0500

    Focus menu items on hover

--- frontend/components/MenuItem.vue
+++ frontend/components/MenuItem.vue
@@ -1,5 +1,11 @@
+<script setup lang="ts">
+function focus(event: PointerEvent) {
+  (event.target as HTMLElement).focus();
+}
+</script>
+
 <template>
-  <ListPanelItem role="menuitem" tabindex="-1">
+  <ListPanelItem role="menuitem" tabindex="-1" @pointermove="focus">
     <slot></slot>
   </ListPanelItem>
 </template>
