5265b3c17b44c0e22c2088a2c3d6262cd3e1189c
Grant Gryczan <grantgryczan@gmail.com>
2026-01-23 00:29:33 -0500

    Reuse CAPTCHA verification API connections

--- backend/api/captcha.rs
+++ backend/api/captcha.rs
@@ -10,15 +10,16 @@ static SECRET_KEY: LazyLock<String> = LazyLock::new(|| {
         .expect("environment variable `TURNSTILE_SECRET_KEY` should be a valid string")
 });
 
+/// The client for connecting to the CAPTCHA verification API.
+static CAPTCHA_CLIENT: LazyLock<reqwest::Client> = LazyLock::new(|| reqwest::Client::new());
+
 /// Returns whether a Cloudflare Turnstile token is valid.
 ///
 /// # Errors
 ///
 /// Returns an error if the verification request fails or cannot be processed.
 pub(crate) async fn verify(token: &str) -> Result<bool, reqwest::Error> {
-    let client = reqwest::Client::new();
-
-    let outcome: Value = client
+    let outcome: Value = CAPTCHA_CLIENT
         .post("https://challenges.cloudflare.com/turnstile/v0/siteverify")
         .json(&json!({
             "secret": *SECRET_KEY,
