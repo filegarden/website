75027bf3a37bd89584ed4d29e15faeeea0aaedb3
Grant Gryczan <grantgryczan@gmail.com>
2026-02-26 03:48:53 -0500

    Support file processing in DB

--- migrations/20240522023049_initialize.sql
+++ migrations/20240522023049_initialize.sql
@@ -71,25 +71,78 @@ CREATE TABLE sessions (
 CREATE INDEX sessions_by_accessed_at ON sessions (accessed_at);
 CREATE INDEX sessions_by_user ON sessions (user_id);
 
-CREATE TYPE encoding AS ENUM ('br');
+CREATE TYPE encoding AS ENUM ('br', 'lep', 'wv');
 
-CREATE TABLE files (
+CREATE TABLE file_contents (
     created_at timestamptz(3) NOT NULL DEFAULT now(),
     modified_at timestamptz(3) NOT NULL DEFAULT now(),
     id bytea PRIMARY KEY,
+    complete boolean NOT NULL,
+    partial_hash bytea
+        CHECK ((partial_hash IS NULL) = complete),
+    hash bytea
+        CHECK ((hash IS NULL) = NOT complete),
+    original_size bigint NOT NULL,
+    encoding encoding,
+    encoded_size bigint NOT NULL DEFAULT 0,
+    part_count integer NOT NULL DEFAULT 0,
+    decoded_part_size integer
+        CHECK (
+            decoded_part_size IS NULL
+            OR NOT complete
+            OR part_count = original_size / decoded_part_size
+        ),
+    decoded_part_sizes integer[]
+        CHECK (
+            decoded_part_sizes IS NULL
+            OR part_count = cardinality(decoded_part_sizes)
+        )
+        CHECK (decoded_part_sizes IS NULL OR decoded_part_size IS NULL),
+
+    UNIQUE (id, complete, original_size)
+);
+
+CREATE INDEX file_contents_by_hash ON file_contents (hash);
+
+CREATE TABLE files (
+    created_at timestamptz(3) NOT NULL,
+    modified_at timestamptz(3) NOT NULL DEFAULT now(),
+    id bytea NOT NULL,
+    complete boolean NOT NULL DEFAULT FALSE,
     name text NOT NULL,
     owner_id bytea NOT NULL REFERENCES users (id) ON DELETE CASCADE,
     parent_id_path bytea[] NOT NULL,
     parent_name_path text[] NOT NULL,
-    shared boolean NOT NULL DEFAULT FALSE,
-    parts integer NOT NULL DEFAULT 1,
     size bigint NOT NULL,
-    encoded_size bigint NOT NULL,
-    encoding encoding,
+    content_id bytea NOT NULL,
     type text NOT NULL,
+    shared boolean NOT NULL DEFAULT FALSE,
 
-    UNIQUE (owner_id, parent_id_path, id),
-    UNIQUE (owner_id, parent_name_path, name)
+    PRIMARY KEY (id, complete),
+    FOREIGN KEY (content_id, complete, size)
+        REFERENCES file_contents (id, complete, original_size)
+        MATCH FULL
+        ON UPDATE CASCADE,
+    UNIQUE (owner_id, parent_id_path, id, complete),
+    UNIQUE (owner_id, parent_name_path, name, complete)
+);
+
+CREATE INDEX files_by_content_id ON files (content_id);
+
+CREATE TABLE files_processing (
+    created_at timestamptz(3) NOT NULL DEFAULT now(),
+    required_before_deduping_file_id bytea UNIQUE
+        REFERENCES files (id) ON DELETE CASCADE
+        CHECK ((required_before_deduping_file_id IS NULL) !=
+            (source_content_hash IS NULL)),
+    source_content_hash bytea UNIQUE,
+    encoding encoding NOT NULL,
+    output_content_id bytea UNIQUE REFERENCES file_contents (id),
+    failed_at timestamptz(3)
+        CHECK ((failed_at IS NULL) != (output_content_id IS NULL)),
+
+    UNIQUE NULLS NOT DISTINCT
+        (required_before_deduping_file_id, source_content_hash, encoding)
 );
 
 CREATE TABLE folders (
