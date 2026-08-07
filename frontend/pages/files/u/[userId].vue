422610636b420bb1f00485734cca3991849a8c86
Grant Gryczan <grantgryczan@gmail.com>
2026-08-07 08:45:11 -0400

    Add sidebar nav to files page

--- frontend/pages/files/u/[userId].vue
+++ frontend/pages/files/u/[userId].vue
@@ -17,7 +17,7 @@ useTitle("Files");
     <div id="account-menu-wrapper"></div>
 
     <div class="below-header">
-      <nav class="sidebar">
+      <div class="sidebar">
         <SplitButton
           class="upload-button"
           menu-label="More Upload Options"
@@ -33,7 +33,9 @@ useTitle("Files");
           <hr />
           <MenuItem><IconFolderPlus />New folder</MenuItem>
         </SplitButton>
-      </nav>
+
+        <nav class="sidebar-nav"></nav>
+      </div>
 
       <div class="file-pane"></div>
     </div>
@@ -52,6 +54,7 @@ $sidebar-width: 16rem;
 .header {
   width: 100%;
   height: 4rem;
+  flex-shrink: 0;
 
   display: flex;
   align-items: center;
@@ -78,17 +81,34 @@ $sidebar-width: 16rem;
 
 .below-header {
   flex-grow: 1;
+  // Override the default of `auto` which can cause overflow.
+  min-height: 0;
+
   display: flex;
 }
 
 .sidebar {
+  display: flex;
+  flex-direction: column;
+
   width: $sidebar-width;
-  padding: 1rem;
 }
 
 .upload-button {
-  width: 100%;
   font-size: 1.25rem;
+  margin: 1rem;
+}
+
+.sidebar-nav {
+  flex-grow: 1;
+
+  overflow-y: auto;
+  scrollbar-color: transparent transparent;
+  scrollbar-width: thin;
+
+  &:hover {
+    scrollbar-color: auto;
+  }
 }
 
 .file-pane {
